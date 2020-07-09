e24425b2ae5433adce0ce10b0462d613f31f4a8b
Kronykus <MoreheadSteve@gmail.com>
2020-07-08 21:46:45 -0500

    14_cal.py complete

--- src/14_cal.py
+++ src/14_cal.py
@@ -26,7 +26,68 @@ This would mean that from the command line you would call `python3 14_cal.py 4 2
 print out a calendar for April in 2015, but if you omit either the year or both values, 
 it should use today’s date to get the month and year.
 """
-
+#########################################
+# Imports
 import sys
 import calendar
 from datetime import datetime
+
+#########################################
+# Functions
+def show_usage():
+    
+    print( "usage:" )
+    print( "    python 14_cal.py month year" )
+    print( "\tmonth - integer 1-12 inclusive" )
+    print( "\tyear  - four digit integer" )
+    print( "" )
+    print( "    * Month and year are both optional" )
+    print( "    * Month can be used without year - will default to current year" )
+    print( "    * If year is provided, month must also be provided, and month must come first" )
+
+######
+def num_check( num ):
+
+    value = 0
+    
+    try:
+        value = int( num )
+    except:
+        show_usage()
+        exit()
+
+    if value < 999 or value > 9999:
+        show_usage()
+        exit()
+
+    return value
+#########################################
+# Setup
+num_args = len( sys.argv )
+
+dt = datetime.now()
+
+calendar.setfirstweekday( 6 )
+
+#########################################
+# Check number of arguments and execute
+if num_args < 2:
+
+    print( calendar.month( dt.year, dt.month ) )
+
+elif num_args == 2:
+
+    month = num_check( sys.argv[ 1 ] )
+
+    print( calendar.month( dt.year, month ) )
+
+elif num_args == 3:
+
+    month = num_check( sys.argv[ 1 ] )
+    year  = num_check( sys.argv[ 2 ] )
+
+    print( calendar.month( year, month ) )
+
+else:
+    show_usage()
+#########################################EoF
